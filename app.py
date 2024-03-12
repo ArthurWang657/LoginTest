34bfc391dc3c00422eb0c25e9b9d619022a85320
ArthurWang657 <66664756+ArthurWang657@users.noreply.github.com>
2024-03-12 16:49:08 +0800

    First Step
    建置基本功能及畫面，尚未連接SQL

---
+++ app.py
@@ -0,0 +1,23 @@
+import flask
+from flask import render_template, request
+app = flask.Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('LoginPage.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    email = request.form['email']
+    print("post : email => ", email)
+    username = request.form['username']
+    print("post : username => ", username)
+    password = request.form['password']
+    print("post : password => ", password)
+    
+    return "SUCCESS!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
